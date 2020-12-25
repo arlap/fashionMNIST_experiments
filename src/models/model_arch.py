1c3f4aaf4edb6d63bfda48781acc89cff59c16cd
Artem Laptev <31847189+arlap@users.noreply.github.com>
2020-12-25 16:42:36 +0300

    modify models

--- src/models/model_arch.py
+++ src/models/model_arch.py
@@ -4,7 +4,9 @@ from keras import Input, layers, Model
 def dense_model():
     input_tensor = Input(shape=(28, 28, 1))
     x = layers.Flatten()(input_tensor)
+    x = layers.Dropout(0.3)(x)
     x = layers.Dense(128, activation='relu')(x)
+    x = layers.Dropout(0.3)(x)
     output_tensor = layers.Dense(10, activation='softmax')(x)
     model = Model(input_tensor, output_tensor)
     return model
@@ -16,14 +18,16 @@ def conv_model():
                       kernel_initializer='he_normal')(input_tensor)
     x = layers.MaxPooling2D(pool_size=(2, 2))(x)
     x = layers.Dropout(0.25)(x)
-    x = layers.Conv2D(64, (3, 3), activation='relu')(x)
+    x = layers.Conv2D(64, (3, 3))(x)
+    x = layers.Activation('relu')(x)
+    x = layers.BatchNormalization()(x)
     x = layers.MaxPooling2D(pool_size=(2, 2))(x)
     x = layers.Dropout(0.25)(x)
     x = layers.Conv2D(128, (3, 3), activation='relu')(x)
     x = layers.Dropout(0.4)(x)
     x = layers.Flatten()(x)
     x = layers.Dense(128, activation='relu')(x)
-    x = layers.Dropout(0.3)(x)
+    x = layers.Dropout(0.4)(x)
     output_tensor = layers.Dense(10, activation='softmax')(x)
     model = Model(input_tensor, output_tensor)
     return model
